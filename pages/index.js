de808543646af7f376540cc8896eb2ac2656bd6b
Seongjun kim <seongjun@condos.ca>
2019-07-06 11:31:51 -0400

    alias added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Header from '@Component/Header'
 
 const Home = () => {
-  return <div>This is home page</div>
+  return (
+   <div>
+      <Header />
+      <div>This is home page</div>
+   </div>
+  )
 
 }
 export default Home
